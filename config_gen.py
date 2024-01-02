094559cdb2a3438a484334413c8028d502828fae
John Kreno <john.kreno@gmail.com>
2024-01-02 01:16:18 -0500

    Made some changes around config output names

--- config_gen.py
+++ config_gen.py
@@ -2,18 +2,14 @@
 import csv
 from jinja2 import Environment, FileSystemLoader
 
-
-
+# Open CSV and read the contents into routers
 with open('config_variables.csv', 'r') as file:
     csv_reader = csv.DictReader(file)
     routers = [row for row in csv_reader]
 
-
 # Open CSV for reading 
-#file = open('config_variables.csv')
-#csvreader = csv.DictReader(file)
-
-
+# file = open('config_variables.csv')
+# csvreader = csv.DictReader(file)
 
 # Extract CSV header
 # header = []
@@ -24,31 +20,25 @@ with open('config_variables.csv', 'r') as file:
 # for router in csvreader:
 #     routers.append(router) 
 
-
 # Close CSV
 # file.close()
 
-
+# Test the contents of routers
 print(routers)
 
-max_score = 100
-test_name = "Python Challenge"
-students = [
-	{"name": "Sandrine", "score": 100},
-	{"name": "Gergeley", "score": 87},
-	{"name": "Frieda", "score": 92},
-]
 
+# Template replacement 
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("config")
 
 for router in routers:
-    filename = f"configs/config_{router['Hostname'].lower()}.txt"
+    filename = f"configs/{router['Hostname'].lower()}.config"
     content = template.render(
         router,
         Hostname=router['Hostname'].lower(),
         IP=router['IP']
     )
-    with open(filename, mode="w", encoding="utf-8") as message:
-        message.write(content)
-        print(f"... wrote {filename}")
+    with open(filename, mode="w", encoding="utf-8") as config:
+        config.write(content)
+        print(f"wrote config for " + router['Hostname'])
+#        print(f"wrote config {filename}")
