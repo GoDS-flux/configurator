b1bc38aa28cc3e3139e593d72ae37d5457063f35
John Kreno <john.kreno@gmail.com>
2024-01-02 13:56:36 -0500

    Cleaned up extraneous code

--- config_gen.py
+++ config_gen.py
@@ -7,25 +7,8 @@ with open('config_variables.csv', 'r') as file:
     csv_reader = csv.DictReader(file)
     routers = [row for row in csv_reader]
 
-# Open CSV for reading 
-# file = open('config_variables.csv')
-# csvreader = csv.DictReader(file)
-
-# Extract CSV header
-# header = []
-# header = next(csvreader) 
-
-# Extract Router Records 
-# routers = []
-# for router in csvreader:
-#     routers.append(router) 
-
-# Close CSV
-# file.close()
-
 # Test the contents of routers
 print(routers)
-
 
 # Template replacement 
 environment = Environment(loader=FileSystemLoader("templates/"))
@@ -41,4 +24,3 @@ for router in routers:
     with open(filename, mode="w", encoding="utf-8") as config:
         config.write(content)
         print(f"wrote config for " + router['Hostname'])
-#        print(f"wrote config {filename}")
